c811e8ceb2e2e9cd87754fba3ef75d1a824d6c6d
axrorovms <msodiq0525@gmail.com>
2023-12-09 16:04:37 +0500

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from cx_Freeze import setup, Executable
+
+setup(
+    home="RPS GAME",
+    wersion="1.0",
+    author="None",
+    description="RPS GAME",
+    executables=[
+        Executable("C:/Users/HP/PycharmProjects/cx-freeze/main.py",
+                   icon="C:/Users/HP/PycharmProjects/cx-freeze/photo_2023-12-09_15-19-34.ico")]
+
+)
